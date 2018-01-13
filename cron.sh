0dcf16ee0322124824d69ae8ebeebace7797bc9b
Florin Godard <fgodard@gmail.com>
2018-01-13 19:25:53 +0000

    Added cron wrapper, removed old bash script, updated gitignore

---
+++ cron.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# check if filename is a symlink
+if [[ -L "${BASH_SOURCE[0]}" ]]; then
+	cd $(dirname $(readlink "${BASH_SOURCE[0]}"))
+else
+	cd $(dirname "${BASH_SOURCE[0]}")
+fi
+
+if [ "$(uname -s)" = "Darwin" ]; then
+	export PATH=/usr/local/opt/python3/bin/:$PATH
+fi
+
+source bin/activate
+TZ=UTC python3 twitterFollowerDiff.py >> cron.log

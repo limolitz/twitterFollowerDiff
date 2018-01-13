ecb16b316a5c35d1ade0c15743377c5e18bd2489
Florin Godard <fgodard@gmail.com>
2018-01-13 19:00:07 +0000

    Added name fetching and sending data to MQTT

--- twitterFollowerDiff.py
+++ twitterFollowerDiff.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
-
-
 import configparser
 import tweepy
 import pickle
 import os.path
+import json
+import subprocess
 
 def getFollowerDiff():
 	api = auth()
 	currentFollowers = getAllFollowersUnordered(api)
 	oldFollowers = getOldFollowers()
-	diff = calcFollowerDiff(oldFollowers,currentFollowers)
+	diff = calcFollowerDiff(oldFollowers,currentFollowers,api)
 	storeFollowers(currentFollowers)
+
+	writeDataToMQTT(currentFollowers,diff)
 
 def storeFollowers(currentFollowers):
 	pickle.dump(currentFollowers, open( "followers.p", "wb" ))
@@ -22,11 +24,44 @@ def getOldFollowers():
 	else:
 		return set()
 
-def calcFollowerDiff(oldFollowers, currentFollowers):
+def getNamesOf(userList,api):
+	names = []
+	for userId in userList:
+		name = api.get_user(userId).screen_name
+		names.append(name)
+	return names
+
+def calcFollowerDiff(oldFollowers, currentFollowers,api):
 	newFollowers = currentFollowers.difference(oldFollowers)
+	newFollowersNames = getNamesOf(newFollowers,api)
 	print("{} new followers.".format(len(newFollowers)))
 	unfollowers = oldFollowers.difference(currentFollowers)
 	print("{} unfollowers.".format(len(unfollowers)))
+	unfollowersNames = getNamesOf(unfollowers,api)
+	return [newFollowersNames,unfollowersNames]
+
+def writeDataToMQTT(currentFollowers,diff):
+	newFollowers = list(diff[0])
+	unfollowers = list(diff[1])
+
+	config = getConfig()
+
+	mqttObject = {
+		"topic": "twitterFollower",
+		"name": config.get("user", "username"),
+		"measurements": {
+			"newFollowers": newFollowers,
+			"unfollowers": unfollowers,
+			"totalFollowers": len(currentFollowers)
+		}
+	}
+
+	mqttString = json.dumps(mqttObject)
+	print("Writing JSON: {}".format(mqttString))
+	sender = subprocess.Popen([config.get("paths", "mqttPath")], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+	output, errors = sender.communicate(mqttString.encode("utf-8"))
+	print(output,errors)
+
 
 def getAllFollowersUnordered(api):
 	config = getConfig()

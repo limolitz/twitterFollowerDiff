8e0528e840f81d50e21d445340aff91aeef4f5ec
Florin Godard <fgodard@gmail.com>
2018-01-13 18:09:31 +0000

    Added error handling, also added logging in case of a bug where all followers are displayed

--- twitterFollowerDiff.sh
+++ twitterFollowerDiff.sh
@@ -1,19 +1,46 @@
 #!/bin/bash
 cd /home/florin/bin
-/usr/local/bin/t set active wasMitNetPi > /dev/null
-/usr/local/bin/t followers wasMitNetzen | sort > followers_new.txt
-followerDiff=$(/usr/bin/comm -23 followers_new.txt followers.txt)
-charCount=${#followerDiff}
+/usr/local/bin/t set active wasMitNetPi >/dev/null 2>/dev/null
+if [ $? -eq 0 ]; then
+	followersNew=$(/usr/local/bin/t followers wasMitNetzen 2>/dev/null)
+	if [ $? -eq 0 ]; then
+		echo "$followersNew" | sort > followers_new.txt
+		followerDiff=$(/usr/bin/comm -23 followers_new.txt followers.txt)
+		charCount=${#followerDiff}
 
-if [ $charCount -gt 1 ]; then
-	# /bin/echo "New Follower: @$followerDiff"
-  fi
+		if [ $charCount -gt 1 ]; then
+			if [ $charCount -gt 400 ]; then
+				/bin/echo "Error. Too many chars: $charCount" >&2
+				wc followers.txt >&2
+				wc followers_new.txt >&2
+				cp followers.txt followers_bug_$(date "+%s").txt
+				cp followers_new.txt followers_new_bug_$(date "+%s").txt
+			else
+				/bin/echo "New Follower: @$followerDiff"
+			fi
+		fi
 
-followerDiff=$(/usr/bin/comm -13 followers_new.txt followers.txt)
-charCount=${#followerDiff}
+		followerDiff=$(/usr/bin/comm -13 followers_new.txt followers.txt)
+		charCount=${#followerDiff}
 
-if [ $charCount -gt 1 ]; then
-	/bin/echo "New Unfollower: @$followerDiff"
+		if [ $charCount -gt 1 ]; then
+			if [ $charCount -gt 400 ]; then
+				/bin/echo "Error. Too many chars: $charCount" >&2
+				wc followers.txt >&2
+        wc followers_new.txt >&2
+        cp followers.txt followers_bug_$(date "+%s").txt
+				cp followers_new.txt followers_new_bug_$(date "+%s").txt
+			else
+				/bin/echo "New Unfollower: @$followerDiff"
+			fi
+		fi
+
+		/bin/mv followers_new.txt followers.txt
+	else
+		#/bin/echo "Getting followers failed." >&2
+		exit 2
+	fi
+else
+	/bin/echo "Setting active user failed." >&2
+	exit 1
 fi
-
-/bin/mv followers_new.txt followers.txt
